d86795c974adb49f810dab3e3ca9410005b4a091
Iqra khan . <ik22082002khan@gmail.com>
2024-12-03 03:29:07 +0000

    bussines name genrator

--- exersice-10/index.js
+++ exersice-10/index.js
@@ -1,1 +1,37 @@
-//bussnies name  Generator
+//bussines name genrators
+
+// adjective:
+// Crazy, Amazing, Fire
+
+// shopName :
+// engine
+// food
+// Garments
+
+// Another Word:
+//  Bros , Limited , Hub
+
+// without array
+
+let adjective = {
+    1: "Crazy",
+    2: "Amazing",
+    3: "Fire",
+  };
+  let shopName = {
+    1: "engine",
+    2: "food",
+    3: "Garments",
+  };
+  let anotherWord = {
+    1: "Bros",
+    2: "Limited",
+    3: "Hub",
+  };
+  
+  let ran1 = Math.floor(Math.random() * 3 + 1);
+  let ran2 = Math.floor(Math.random() * 3 + 1);
+  let ran3 = Math.floor(Math.random() * 3 + 1);
+  
+  console.log(`${adjective[ran1]} ${shopName[ran2]} ${anotherWord[ran3]}`);
+  
